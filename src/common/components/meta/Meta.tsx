c2f5279626fefcdbd3e24873757dfa16e0c373da
Charles Sin <charlessin1993@gmail.com>
2023-04-08 12:50:24 +0800

    1. update seo meta tag & info.

--- src/common/components/meta/Meta.tsx
+++ src/common/components/meta/Meta.tsx
@@ -2,9 +2,11 @@ const Meta = () => {
   return (
     <>
       <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
-      <link rel="canonical" href="https://charles-sin.vercel.app/"></link>
-      <link rel="pingback" href="https://charles-sin.vercel.app/"></link>
-      <link rel="alternate" href="https://charles-sin.vercel.app/" hrefLang="en-us" />
+      <link rel="apple-touch-icon" sizes="32x32" href="./images/favicon32.ico"></link>
+      <link rel="apple-touch-icon" sizes="96x96" href="./images/favicon96.ico"></link>
+      <link rel="canonical" href="https://charles-sin.vercel.app"></link>
+      <link rel="pingback" href="https://charles-sin.vercel.app"></link>
+      <link rel="alternate" href="https://charles-sin.vercel.app" hrefLang="en-us"></link>
 
       <title> Charles Sin.C.Y : Portfolio </title>
 
@@ -22,13 +24,12 @@ const Meta = () => {
         property="og:description"
         content="Web Development Company, Web Development Services, Website Development Company, Website Development Services, Website Development Taiwan, Web Development Taiwan, Company, Services, Taiwan"
       />
-      <meta property="og:url" content="https://charles-sin.vercel.app/" />
+      <meta property="og:url" content="https://charles-sin.vercel.app" />
       <meta property="og:image" content="https://charles-sin.vercel.app/images/cover.jpg" />
       <meta property="og:type" content="website" />
 
       <meta name="google-site-verification" content="eP82dEE--Mtf-ypVN9QO-B9kd-C5uLVNT_Ce_bdha24" />
-
-      <meta property="article:publisher" content="https://www.facebook.com/Charles" />
+      <meta property="article:publisher" content="Charles" />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@Charles" />
