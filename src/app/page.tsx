7cc5e715890ec013f3bf5089f023cae10ffef15c
Charles Sin <charlessin1993@gmail.com>
2023-05-20 15:53:39 +0800

    1. add console toi print vercel process.env variable.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,14 @@ const Page = () => {
   useEffect(() => {
     async function fetchData() {
       const URL = process.env.ENV === "dev" ? `${process.env.DEV_DOMAIN}/api/userlog` : `${process.env.PRD_DOMAIN}/api/userlog`;
+      const URI = "https://charles-sin.vercel.app/api/userlog";
+      
       console.log({ URL });
-      const { data } = await axios.get(URL);
+      console.log("process.env.ENV: " + process.env.ENV);
+      console.log("process.env.DEV_DOMAIN: " + process.env.DEV_DOMAIN);
+      console.log("process.env.PRD_DOMAIN: " + process.env.PRD_DOMAIN);
+
+      const { data } = await axios.get(URI);
       console.log("Component will be unmount");
       console.log({ data });
     }
