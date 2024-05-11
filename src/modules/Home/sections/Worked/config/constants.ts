74744b0b3cf8981381b971f944f3add4db43f6e6
Charles Sin <charlessin1993@gmail.com>
2024-05-11 21:15:09 +0800

    update worked information

--- src/modules/Home/sections/Worked/config/constants.ts
+++ src/modules/Home/sections/Worked/config/constants.ts
@@ -4,7 +4,23 @@ import { WorkedTypes } from "./types";
 export const worked_data: WorkedTypes[] = [
   {
     id: 0,
-    label: "Frontend",
+    label: "Senior Engineer",
+    description: [
+      "Responsible for building, configuring and maintaining continuous integration/continuous delivery CI/CD pipeline to improve the efficiency of software development and deployment",
+      "Design and implement automation tools and scripts to simplify infrastructure deployment, configuration and management processes",
+      "Assist the development team to implement agile development practices and achieve rapid iteration and deployment through continuous integration and continuous delivery",
+      "Integrate and optimize various tools and technologies such as containerization Docker, Kubernetes, configuration management Terraform, Terragrunt and monitoring Datadog, Grafana for automation and process improvement",
+      "Provide technical support and training to help team members better understand and leverage DevOps processes and tools",
+    ],
+    // present
+    deadline: "Jun 20 - Apr 23",
+    title: "Senior Engineer",
+    company: "Supebtech.asia",
+    url: "https://caino.ai/",
+  },
+  {
+    id: 1,
+    label: "Frontend Developer",
     description: [
       "Write modern, performant, maintainable code for a diverse array of client and internal projects",
       "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
@@ -15,8 +31,8 @@ export const worked_data: WorkedTypes[] = [
       "Integrate and optimize various tools and technologies such as containerization (Docker, Kubernetes), configuration management (Terraform, Terragrunt), and monitoring (Datadog, Grafana) to achieve automation and process improvement",
       "Provide technical support and training to help team members better understand and leverage DevOps processes and tools",
     ],
-    deadline: "Sep 20 - present",
-    title: "Frontend developer",
+    deadline: "Sep 20 - Jun 23",
+    title: "Frontend Developer",
     company: "Supebtech.asia",
     url: "https://caino.ai/",
   },
